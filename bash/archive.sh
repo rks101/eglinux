f55d2027b6fb58ac3cb31275be2a2c3f3fb8275b
rks <rajendra.kr.solanki@gmail.com>
2026-09-15 16:43:55 +0530

    Update archive.sh

--- bash/archive.sh
+++ bash/archive.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+# Directory to create archive file for 
 dir="logs"
 
+# Double quotes around $dir and again around _`date`.tar.gz are important. 
 arch_file="$dir""_`date +%d_%m_%Y_%H_%M_%S`.tar.gz"
 
 echo $arch_file
 
+# tar with -f for archive file and -z flag for .tar.gz (gzip compression) 
 tar -zcvf $arch_file $dir
