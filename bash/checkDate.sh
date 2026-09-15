a00c489890ec9cf6aff813fbe52ec19849e065a7
rks <rajendra.kr.solanki@gmail.com>
2026-09-15 16:48:52 +0530

    Update checkDate.sh

--- bash/checkDate.sh
+++ bash/checkDate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 date=$1
 #format="DD-MM-YYYY"
@@ -9,13 +9,14 @@ date=$1
 # Grouping values
 regexDate="([0-9]{2})-([0-9]{2})-([0-9]{4})";
 
-if [[ $date =~ $regexDate ]]; then
-        echo "Date is valid."
-        day=${BASH_REMATCH[1]}
-        month=${BASH_REMATCH[2]}
-        year=${BASH_REMATCH[3]}
-        echo "Date entered is:"
-        echo "Day: $day, Month: $month, Year: $year"
-else
-        echo "Date entered is NOT valid."
+if [[ $date =~ $regexDate ]]
+  then
+    echo "Date is valid."
+    day=${BASH_REMATCH[1]}
+    month=${BASH_REMATCH[2]}
+    year=${BASH_REMATCH[3]}
+    echo "Date entered is:"
+    echo "Day: $day, Month: $month, Year: $year"
+  else
+    echo "Date entered is NOT valid."
 fi
