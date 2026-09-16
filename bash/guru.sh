5291532c7b906ec7d7ed77bcbe1def8a2ba03ded
rks <rajendra.kr.solanki@gmail.com>
2026-09-16 10:12:33 +0530

    Update guru.sh

--- bash/guru.sh
+++ bash/guru.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # Print information, and a few comparisons    
-
+echo -e "\n\n"
 echo "Starting the script..."
 
 echo -e "User: $USER \n"
 echo -e "Hostname: $HOSTNAME \n"
 echo -e "Path: $PATH \n"
+echo -e "Directories inside the path are:"
+echo $PATH | tr ':' '\n'
 
+echo -e "\n\n"
 echo -e "Filename: $0"
 echo -e "Number of arguments to this script = $#" 
 #echo -e "Arguments are: $1 $2 $3"
@@ -25,6 +28,8 @@ else
     echo -e "number of arguments = $#"
 fi
 
+echo -e "\n\n"
+
 file1="SID.txt"
 file2="abc.out"
 
@@ -33,6 +38,8 @@ if [ -e $file1 ]
     echo -e "$file1 exists."
   else 
     echo -e "$file1 does not exist."
+    touch $file1 
+    echo -e "empty $file1 is created."
 fi 
 
 if [ -e $file2 ]
@@ -40,7 +47,11 @@ if [ -e $file2 ]
     echo -e "$file2 exists."
   else 
     echo -e "$file2 does not exist."
+    touch $file2
+    echo -e "empty $file2 is created."
 fi
+
+echo -e "\n\n"
 
 if [ $file1 -nt $file2 ]
   then
@@ -49,7 +60,9 @@ else
     echo -e "$file2 is newer than $file1"
 fi
 
-echo "Press any key "
+echo -e "\n\n"
+
+echo "Press any key followed by the enter key"
 read key_value
 echo "$key_value"
 
